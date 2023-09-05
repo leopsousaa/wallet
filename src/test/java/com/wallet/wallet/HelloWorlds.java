6a94c386733e3da162e48bcb0f0061aa643cd6cf
lesousa <lesousa@brasilseg.com.br>
2023-09-05 01:54:15 -0300

    add first test

---
+++ src/test/java/com/wallet/wallet/HelloWorlds.java
@@ -0,0 +1,15 @@
+package com.wallet.wallet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloWorlds {
+
+  @Test
+  public void textHelloWorld() {
+    assertEquals(1,1);
+  }
+}
